d2cbe92bf26f80955a18f7cccdbe4ac5f39e448d
Harits <mharitsnf@gmail.com>
2023-06-06 21:47:19 +0200

    add diffuse computation

--- common/vec3.h
+++ common/vec3.h
@@ -10,6 +10,9 @@
 
 class vec3{
 public:
+    double e[3];
+
+public:
     vec3() : e{0, 0, 0} {}
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
@@ -43,8 +46,13 @@ public:
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 
-public:
-    double e[3];
+    inline static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    inline static vec3 random(double min, double max) {
+        return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+    }
 };
 
 // Type aliases for vec3
@@ -93,6 +101,18 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
+}
+
+vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1,1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
+vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
 }
 
 #endif //RAYTRACINGINONEWEEKEND_VEC3_H
